6a736bf7895cf48b3089ec4bdab880a766558b84
Vincent Garrigues <vincent@garriguv.io>
2020-06-08 12:34:53 +0200

    Add VS code defaults

--- defaults.sh
+++ defaults.sh
@@ -2,3 +2,6 @@
 
 # Default screenshots directory
 defaults write com.apple.screencapture location ~/Downloads
+
+# Enable repeat keypresses in VS Code
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
